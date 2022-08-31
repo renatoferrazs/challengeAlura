17e50421882d0de255165f19777a887177b7f111
RenatoFerrazs <renatoferrazs@hotmail.com>
2022-08-30 23:02:45 -0300

    acertando estruturas

---
+++ controle-orcamento-familiar/src/test/java/br/com/alura/controle/orcamento/familiar/DespesaTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.controle.orcamento.familiar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DespesaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented.");
+	}
+	
+}
