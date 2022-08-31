2eb897f30fc9bab02ab7a802ac1bb3c4ed796df2
RenatoFerrazs <renatoferrazs@hotmail.com>
2022-08-30 22:39:34 -0300

    adicionando novos arquivos

---
+++ controle-orcamento-familiar/src/test/java/br/com/alura/controle/orcamento/familiar/ReceitaTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.controle.orcamento.familiar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ReceitaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented.");
+	}
+	
+}
