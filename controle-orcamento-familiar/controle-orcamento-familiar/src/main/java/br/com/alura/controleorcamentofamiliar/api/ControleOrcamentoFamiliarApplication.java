b2447ba4f1e30ab18353e93f109fe62360bb85ff
RenatoFerrazs <renatoferrazs@hotmail.com>
2022-09-09 17:47:01 -0300

    refatorando toda a estrutura do projeto para uma api

---
+++ controle-orcamento-familiar/controle-orcamento-familiar/src/main/java/br/com/alura/controleorcamentofamiliar/api/ControleOrcamentoFamiliarApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.controleorcamentofamiliar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleOrcamentoFamiliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleOrcamentoFamiliarApplication.class, args);
+	}
+
+}
---
+++ controle-orcamento-familiar/controle-orcamento-familiar/src/test/java/br/com/alura/controleorcamentofamiliar/api/ControleOrcamentoFamiliarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.controleorcamentofamiliar.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleOrcamentoFamiliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
